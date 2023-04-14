2c23fc6050c8c1643f3e9ba894551df27e322134
imaymarques <mayaramarquescosta@outlook.com>
2023-04-14 17:45:36 -0300

    feat: Creating search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+from datetime import datetime
 
 
 # Requisito 7
@@ -16,7 +17,14 @@ def search_by_title(title: str):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        br_date = datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
+        return [
+            (news["title"], news["url"])
+            for news in search_news({"timestamp": {"$eq": br_date}})
+        ]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
