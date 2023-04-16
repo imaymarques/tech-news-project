6519a26185824d205f3efe46ea87c8074e4b1861
imaymarques <mayaramarquescosta@outlook.com>
2023-04-16 16:53:14 -0300

    feat: Creating search_by_category function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -29,4 +29,6 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    regex = re.compile(category, re.IGNORECASE)
+    query = {'category': regex}
+    return [(news["title"], news["url"]) for news in search_news(query)]
