edb0d4a91625f2f83fc4176cb9d1fa5052a641c1
imaymarques <mayaramarquescosta@outlook.com>
2023-04-16 17:12:36 -0300

    feat: Creating top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,13 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {"$sortByCount": "$category"},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    categories = [doc["_id"] for doc in get_collection().aggregate(pipeline)]
+
+    return categories
